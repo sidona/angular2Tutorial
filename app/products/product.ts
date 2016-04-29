236782bb65b9769e1ac001dd7507f90fa6900bdb
sidona <sidona_g@yahoo.com>
2016-04-29 17:40:29 +0300

    tutorial pluralsight

---
+++ app/products/product.ts
@@ -0,0 +1,17 @@
+/**
+ * Created by sdonose on 4/29/2016.
+ */
+
+
+export interface IProduct {
+    productId:number;
+    productName:string;
+    productCode:string;
+    releaseDate:string;
+    price:number;
+    description:string;
+    starRating:number;
+    imageUrl:string;
+}
+
+
